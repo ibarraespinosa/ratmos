7439c318f1e0dbbf8640171b53bb588eae9e0091
ibarraespinosa <zergioibarra@gmail.com>
2018-09-13 19:52:56 -0300

    0.1.1 improving raster_wrf

--- R/raster_wrf.R
+++ R/raster_wrf.R
@@ -3,12 +3,6 @@
 #' @description \code{\link{raster_nc}} Creates raster from nc path
 #'
 #' @param nc Character; Path to NetCDF.
-#' @param xmin Numeric; xmin coordinate.
-#' @param xmax Numeric; xmax coordinate.
-#' @param ymin Numeric; ymin coordinate.
-#' @param ymax Numeric; ymax coordinate.
-#' @param dx Numeric; grid spacing x.
-#' @param dy Numeric; grid spacing y.
 #' @param array Logical; return array or not.
 #' @param var Character; name of variable. If you dont know, you can choose.
 #' @param z Integer; Level (asusming third dimencion)
@@ -20,11 +14,7 @@
 #' @examples \dontrun{
 #' #dont run
 #' }
-raster_wrf <- function(nc,
-                      xmin = -150, xmax = 10,
-                      ymin = -60, ymax = 40,
-                      dx = 215, dy = 135,
-                      var, z = 6,
+raster_wrf <- function(nc, var, z,
                       array = FALSE, verbose = TRUE){
   f <- ncdf4::nc_open(filename = nc)
   if(verbose){
@@ -48,6 +38,16 @@ raster_wrf <- function(nc,
   }
   if(array){
     return(u)
+  }
+
+  if(missing(z)){
+    choice <- utils::menu(1:dim(u)[3], title="Choose Level Z")
+    z <- (1:dim(u)[3])[choice]
+    if(verbose){
+      cat(paste0("\nThe level is'", z, "\n"))
+    }
+  } else {
+    z = z
   }
     lat <- ncdf4::ncvar_get(f, "XLAT" )
     lon <- ncdf4::ncvar_get(f, "XLONG" )
---
+++ R/ratmos.R
@@ -0,0 +1,9 @@
+#' ratmos: a package to read gridded model outputs, convert them to raster and plot
+#'
+#' This package is designed to have few dependencies, raster, ncdf4 and sp..
+#' The functions are designed so that, you only enter with the  path of the netcdf file
+#' and then you can choose variables and levels interactivly
+#'
+#' @docType package
+#' @name ratmos
+NULL
