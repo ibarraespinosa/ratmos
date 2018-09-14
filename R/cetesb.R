363cc9d6f811b58bd265c737a6d63c3c8a240979
ibarraespinosa <zergioibarra@gmail.com>
2018-09-13 21:28:02 -0300

    0.1.1 improving raster_wrf

---
+++ R/cetesb.R
@@ -0,0 +1,10 @@
+#' Location some air quality stations CETESB
+#'
+#' @format A SpatialPointsDataFrame with 23 stations
+#' \describe{
+#'   \item{estacion}{Station}
+#' }
+#' @source \url{http://cetesb.sp.gov.br/ar/publicacoes-relatorios/}
+#' @usage data(cetesb)
+#' @docType data
+"cetesb"
