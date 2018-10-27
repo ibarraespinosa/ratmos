4003c043d07a1fe95688df03c8b34d985e093b9a
salvatirehbein <salvati.rehbein@gmail.com>
2018-10-27 13:49:53 -0300

    Add wind shear fix #1

---
+++ NEWS.md
@@ -0,0 +1,14 @@
+NEWS
+===========
+
+# ratmos 0.1.4 (Release date: 2018-10-27)
+
+- Add wind_shear
+
+## ratmos 0.1.3 (Release date: 2018-10-03)
+
+- Add Readme.Rmd
+
+## ratmos 0.1.0 (Release date: 2018-09-07)
+
+- Release ratmos
---
+++ R/wind_shear.R
@@ -0,0 +1,51 @@
+#' Calculates Wind Shear
+#'
+#' @description \code{\link{wind_shear}} Creates raster from nc path
+#'
+#' @param u Numeric;
+#' @param v Numeric;
+#' @param zlev Numeric;
+#' @param at_zlev Numeric;
+#' @param zdiff Numeric;
+#' @param ascending Logical;
+#' @param dataframe Logical;
+#' @export
+#' @examples \dontrun{
+#' #dont run
+#' }
+wind_shear <- function(u,
+                       v,
+                       zlev,
+                       at_zlev,
+                       zdiff,
+                       ascending = TRUE,
+                       dataframe = FALSE) {
+  if(!missing(at_zlev)){
+    dff <- data.frame(u, v, zlev)
+    dff <- dff[dff$zlev %in% at_zlev, ]
+    u <- dff$u
+    v <- dff$v
+  }
+  if(ascending){
+    utop <- c(u[2:length(u)], NA)
+    ubottom <- u
+    vtop <- c(v[2:length(v)], NA)
+    vbottom <- v
+  } else {
+    ubottom   <- c(u[2:length(u)], NA)
+    utop <- u
+    vbottom   <- c(v[2:length(v)], NA)
+    vtop <- v
+  }
+  if(missing(zdiff)) {
+    wsh <- sqrt((utop - ubottom)^2 + (vtop - vbottom)^2)
+  } else {
+    wsh <- sqrt((utop - ubottom)^2 + (vtop - vbottom)^2)/zdiff
+  }
+  if(dataframe){
+    df <- as.data.frame(cbind(utop, ubottom, vtop, vbot0tom, wsh))
+    return(df)
+  } else{
+    return(wsh)
+  }
+}
