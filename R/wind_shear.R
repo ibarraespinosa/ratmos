de212e3f89455370bf815d6c69655fa4898554f0
ibarraespinosa <zergioibarra@gmail.com>
2018-10-31 11:41:54 -0300

    info

--- R/wind_shear.R
+++ R/wind_shear.R
@@ -7,54 +7,54 @@
 #' @param zlev Numeric; levels of each wind
 #' @param zlev_bottom Numeric; bottom nivels to calculate wind shear, from zlev
 #' @param zlev_top Numeric; top nivels to calculate wind shear, from zlev
-#' @param dataframe Logical; Do you want a data.frame with all values?
+#' @param list Logical; Do you want a list of data.frames with all values?
 #' @export
 #' @examples \dontrun{
 #' #dont run
 #' }
 wind_shear <- function(u,
-                       v,
-                       zlev,
-                       zlev_bottom,
-                       zlev_top,
-                       dataframe = FALSE) {
+                        v,
+                        zlev,
+                        zlev_bottom,
+                        zlev_top,
+                        list = FALSE) {
   # La idea es comparar niveles base con varios niveles verticales
   lbottom <- length(zlev_bottom)
-  ltop <- length(top)
-  lista <- list()
+  print(lbottom)
+  ltop <- length(zlev_top)
+  print(ltop)
 
   dff <- data.frame(u, v, zlev)
-  ws  <- matrix(NA, ncol = lbottom, nrow = ltop)
-  wsdf  <- matrix(NA, ncol = 6, nrow = lbottom*ltop)
+  ws  <- as.data.frame(matrix(NA, ncol = lbottom, nrow = ltop))
+  row.names(ws) <- zlev_top
+  names(ws) <- zlev_bottom
+  ubottom <- utop <- vbottom <- vtop <- ws
+  print(dim(ws))
   for(i in 1:lbottom){
     for(j in 1:ltop){
       dff_bottom <- dff[dff$zlev == zlev_bottom[i], ]
+      print(dff_bottom)
       dff_top <- dff[dff$zlev == zlev_top[j], ]
-      ws[i,j] <- sqrt((dff_top$u - dff_bottom$u)^2 +
+      print(dff_top)
+
+      ws[j,i] <- sqrt((dff_top$u - dff_bottom$u)^2 +
                         (dff_top$v - dff_bottom$v)^2)
+      ubottom[j, i] <- c(dff_bottom$u)
+      vbottom[j, i] <- c(dff_bottom$v)
+      utop[j, i] <- c(dff_top$u)
+      vtop[j, i] <- c(dff_top$v)
     }
   }
 
-  if(dataframe){
-    wsdf  <- matrix(NA, ncol = 6, nrow = lbottom*ltop)
-    ubottoms <- dff[dff$zlev %in% zlev_bottom, "u"]
-    vbottoms <- dff[dff$zlev %in% zlev_bottom, "v"]
-    utops <- dff[dff$zlev %in% zlev_top, "u"]
-    vtops <- dff[dff$zlev %in% zlev_top, "v"]
-    ub <- rep(ubottoms, each = length(utops)) #!
-    vb <- rep(vbottoms, each = length(vtops)) #!
-    zLevs <- expand.grid(zlev_bottom, zlev_top) #data.frame
-    zLevs <- zLevs[rev(order(zLevs$Var1)), ]
-    zLevs$Var1 <- ifelse(zLevs$Var1 < zLevs$Var2, NA, zLevs$Var1)
-    zLevs <- zLevs[!is.na(zLevs$Var1),]
-    df <- data.frame(
-      u = ub,
-      v = ub,
-      zlev_bottom = zLevs$Var1,
-      zlev_top = zLevs$Var2,
-      ws = as.vector(ws)
-    )
-    return(df)
+  if(list){
+    df <- data.frame(zbottom = rep(zlev_bottom, each = length(zlev_top)))
+    df$ztop <- rep(zlev_top, length(zlev_bottom))
+    df$ubottom <- unlist(ubottom)
+    df$vbottom <- unlist(vbottom)
+    df$utop <- unlist(utop)
+    df$vtop <- unlist(vtop)
+    df$ws <- unlist(ws)
+    return(list(ws, df))
   } else{
     return(as.vector(ws))
   }
